369e0edb6169c5d9636e688745d14575af73c495
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2020-08-07 18:00:24 -0300

    Commit 2 dos arquivos do mobile
    Commit 2 dos arquivos do mobile

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import styles from "./styles";
 import { BorderlessButton } from 'react-native-gesture-handler';
@@ -8,9 +8,10 @@ import logoImage from "../../assets/images/icons/logo.png";
 
 interface PageHeaderProps {
     title: string;
+    headerRight?: ReactNode; // Componente como propriedade
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, headerRight }) => {
     const { navigate } = useNavigation();
 
     function handleGoBack() {
@@ -33,9 +34,13 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
                     resizeMode="contain"
                 />
             </View>
-            <Text style={styles.title}>
-                {title}
-            </Text>
+            <View style={styles.header}>
+                <Text style={styles.title}>
+                    {title}
+                </Text>
+                {headerRight}
+            </View>
+            {children}
         </View>
     );
 }
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
@@ -7,6 +7,7 @@ import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
 import giveClassesIcon from "../../assets/images/icons/give-classes.png";
 import heartIcon from "../../assets/images/icons/heart.png";
+import api from '../../services/api';
 
 function Landing() {
     const { navigate } = useNavigation();
@@ -18,6 +19,16 @@ function Landing() {
     function handleNavigateToStudyPages() {
         navigate('Study');
     }
+
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        api.get('connections')
+            .then(response => {
+                const { total } = response.data;
+                setTotalConnections(total);
+            });
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -45,7 +56,7 @@ function Landing() {
                 </RectButton>
             </View>
             <View style={styles.totalConnections}>
-                Total de 285 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </View>
         </View>
